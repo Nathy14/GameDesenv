29b6f2870014cbd4ee7bd951ffc307409b858143
Natália <natalia.gama.mattos@hotmail.com>
2020-05-04 14:47:29 -0300

    2D Game Engine using SDL2

---
+++ 2D_Game/2D_Game/main.cpp
@@ -0,0 +1,16 @@
+#include "Game.h"
+Game* game = nullptr;
+
+int main(int argc, const char* agrv[]) {
+
+	game = new Game();
+
+	while (game->running()) {
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+	game->clean();
+
+	return 0;
+}
